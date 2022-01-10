8218ce4daf2ecb1f3f038e89ed78a01ebff1e142
MYuPopov <beemax@bk.ru>
2022-01-10 16:07:43 +0300

    Добавлена сортировка

--- js/renderItems.js
+++ js/renderItems.js
@@ -1,4 +1,4 @@
-// console.log('Скрипт подключен');
+/*jshint esversion: 6 */
 
 const allCards = document.querySelector('.all-cards');
 const selectMovies = document.querySelector('.select-movies');
@@ -8,22 +8,14 @@ const getData = (movie = 'All Movies') => {
 		.then((res) => res.json())
 		.then((data) => {
 			const array = [];
-			// console.log(currentElementsId);
-			// for (let keyId of currentElementsId) {
-			// 	for (let keyDataDb of data.db) {
-			// 		if (keyDataDb.id === keyId) {
-			// 			array.push(keyDataDb);
-
 			if (movie === 'All Movies') {
 				data.forEach((item) => {
 					array.push(item);
 				});
 			} else {
 				data.forEach((item) => {
-					if (item.movies) {
-						if (item.movies.find((item) => item === movie)) {
-							array.push(item);
-						}
+					if (item.movies.find((item) => item === movie)) {
+						array.push(item);
 					}
 				});
 			}
@@ -36,70 +28,32 @@ const renderCards = (array) => {
 	array.forEach(({ name, realName, species, citizenship, gender, status, actors, photo, movies }) => {
 		const card = document.createElement('div');
 		card.classList.add('card');
-
 		card.innerHTML = `
     <div class="picture">
       <img class='photo' src=${photo}></img>
     </div>
- 
     <div class="row">
-
-    <div class="column info"> 
-      <div class='name'>
-        <span class='info-title'>name</span>
-        </div>
-      <div class='realName'>
-        <span class='info-title'>realName</span>
-        </div>
-      <div class='species'>
-        <span class='info-title'>species</span>
-      </div>
-      <div class='citizenship'>
-        <span class='info-title'>citizenship</span>
-      </div>
-      <div class='gender'>
-        <span class='info-title'>gender</span>
-      </div>
-      <div class='status'>
-        <span class='info-title'>status</span>
-      </div>
-      <div class='actors'>
-        <span class='info-title'>actors</span>
-      </div>
-      <div class='movies'>
-        <span class='info-title'>movies</span>
+     <div class="column info"> 
+      <div class='name'>name</div>
+      <div class='realName'>realName</div>
+      <div class='species'>species</div>
+      <div class='citizenship'>citizenship</div>
+      <div class='gender'>gender</div>
+      <div class='status'>status</div>
+      <div class='actors'>actors</div>
+      <div class='movies'>movies</div>
+     </div>
+     <div class="column data"> 
+      <div class='name'><b>${name}</b></div>
+      <div class='realName'>${realName}</div>
+      <div class='species'>${species}</div>
+      <div class='citizenship'>${citizenship}</div>
+      <div class='gender'>${gender}</div>
+      <div class='status'>${status}</div>
+      <div class='actors'>${actors}</div>
+      <div class='movies'><b>${movies}</b></div>
      </div>
     </div>
- 
-      <div class="column data"> 
-        <div class='name'>
-          <span class='info-value'><b>${name}</b></span>
-          </div>
-        <div class='realName'>
-          <span class='info-value'>${realName}</span>
-          </div>
-        <div class='species'>
-          <span class='info-value'>${species}</span>
-        </div>
-        <div class='citizenship'>
-          <span class='info-value'>${citizenship}</span>
-        </div>
-        <div class='gender'>
-          <span class='info-value'>${gender}</span>
-        </div>
-        <div class='status'>
-          <span class='info-value'>${status}</span>
-        </div>
-        <div class='actors'>
-          <span class='info-value'>${actors}</span>
-        </div>
-        <div class='movies'>
-          <span class='info-value'><b>
-          ${movies}
-          </b></span>
-       </div>
-      </div>
-      </div>
     `;
 		allCards.append(card);
 	});
